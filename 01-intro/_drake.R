9feb3c462925cc12c1feed1010a035e85cbc1add
pat-s <patrick.schratz@gmail.com>
2019-09-24 19:45:17 +0200

    add screenshots

--- 01-intro/_drake.R
+++ 01-intro/_drake.R
@@ -1,13 +1,11 @@
 # Load packages, functions and plan --------------------------------------------
 
 source("packages.R")
-
 sourceDirectory("R")
 
 # Combine plans ----------------------------------------------------------------
-
-plan = bind_plans(learner_plan, task_plan, param_sets_plan, tuning_plan, train_plan)
+plan = bind_plans(learner_plan, task_plan,
+  param_sets_plan, tuning_plan, train_plan)
 
 # Set the config ---------------------------------------------------------------
-
 drake_config(plan, verbose = 2)
