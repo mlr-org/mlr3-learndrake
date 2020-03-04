0574445797bfc254ad9526397f9a933c5fb72934
pat-s <patrick.schratz@gmail.com>
2020-03-04 20:06:29 +0100

    add ci

---
+++ tic.R
@@ -0,0 +1,7 @@
+# installs dependencies, runs R CMD check, runs covr::codecov()
+get_stage("install") %>%
+  add_step(step_install_deps())
+
+get_stage("script") %>%
+  add_code_step(setwd("02-benchmark")) %>%
+  add_code_step(drake::r_make())
