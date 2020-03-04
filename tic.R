a4eb6108536a2d21553a5201cfa785be75858bda
pat-s <patrick.schratz@gmail.com>
2020-03-04 21:18:34 +0100

    run both examples

--- tic.R
+++ tic.R
@@ -3,5 +3,7 @@ get_stage("install") %>%
   add_step(step_install_deps())
 
 get_stage("script") %>%
-  add_code_step(setwd("02-benchmark")) %>%
+  add_code_step(setwd("01-intro")) %>%
+  add_code_step(drake::r_make()) %>%
+  add_code_step(setwd("../02-benchmark")) %>%
   add_code_step(drake::r_make())
