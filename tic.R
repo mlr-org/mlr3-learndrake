e71213ebbf78121d13a3ad502c210a0519a6aef3
pat-s <patrick.schratz@gmail.com>
2020-03-09 21:50:48 +0100

    try two-fold renv approach

--- tic.R
+++ tic.R
@@ -1,7 +1,12 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
-get_stage("install") %>%
-  #add_step(step_install_deps())
-  add_code_step(renv::restore())
+
+if (ci_has_env("LATEST")) {
+  get_stage("install") %>%
+    add_code_step(renv::hydrate())
+} else {
+  get_stage("install") %>%
+    add_code_step(renv::restore())
+}
 
 get_stage("script") %>%
   add_code_step(setwd("01-intro")) %>%
