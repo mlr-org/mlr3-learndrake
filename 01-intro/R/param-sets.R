c5d5b7630eb29494aa4e8d6d85b80bc7614680d5
pat-s <patrick.schratz@gmail.com>
2020-08-12 07:28:46 +0200

    naming

--- 01-intro/R/param-sets.R
+++ 01-intro/R/param-sets.R
@@ -1,5 +1,5 @@
 param_sets_plan = drake_plan(
-  param_set_rf = target(
+  search_space_rf = target(
     ParamSet$new(params = list(
       ParamInt$new("mtry", lower = 1, upper = 5), # random choice
       ParamInt$new("num.trees", lower = 100, upper = 300) # random choice
--- 01-intro/R/tuning.R
+++ 01-intro/R/tuning.R
@@ -5,7 +5,7 @@ tuning_plan = drake_plan(
       learner = learner_rf, # from learner.R
       resampling = rsmp("holdout"),
       measure = msr("classif.ce"),
-      search_space = param_set_rf, # from param-sets.R
+      search_space = search_space_rf, # from param-sets.R
       terminator = trm("evals", n_evals = 5), # tuning budget
       tuner = tnr("random_search")
     )
